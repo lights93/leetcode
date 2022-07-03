6706785c951aa71953d9a46d812b2cdb027c99a9
minho8615 <lights8615@gmail.com>
2022-07-03 16:42:01 +0900

    Time: 257 ms (23.08%), Space: 37.5 MB (15.38%) - LeetHub

---
+++ 376-wiggle-subsequence/376-wiggle-subsequence.kt
@@ -0,0 +1,25 @@
+class Solution {
+    fun wiggleMaxLength(nums: IntArray): Int {
+        val up = IntArray(nums.size)
+        val down = IntArray(nums.size)
+        up[0] = 1
+        down[0] = 1
+
+        nums.indices
+            .drop(1)
+            .forEach {
+                if(nums[it - 1] > nums[it]) {
+                    down[it] = up[it - 1] + 1
+                    up[it] = up[it - 1]
+                } else if(nums[it -1] < nums[it]) {
+                    up[it] = down[it - 1] + 1
+                    down[it] = down[it - 1]
+                } else {
+                    up[it] = up[it - 1]
+                    down[it] = down[it - 1]
+                }
+            }
+        
+        return maxOf(up[up.lastIndex], down[down.lastIndex])
+    }
+}
